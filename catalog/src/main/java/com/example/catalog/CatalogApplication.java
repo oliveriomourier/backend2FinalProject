9d3a0582ed660c55ca063d01f66324bb5377220f
oliverio <om.mourier@gmail.com>
2022-05-10 11:50:05 -0300

    changes

---
+++ catalog/src/main/java/com/example/catalog/CatalogApplication.java
@@ -0,0 +1,19 @@
+package com.example.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class CatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogApplication.class, args);
+	}
+
+}
